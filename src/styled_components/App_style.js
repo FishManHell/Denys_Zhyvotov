8a6a676db54ffe739f07857ec4ac44da7a4298c7
FishManHell <leonard66693@gmail.com>
2022-01-24 23:26:10 +0200

    change some style

--- src/styled_components/App_style.js
+++ src/styled_components/App_style.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import background from '../images/peakpx.jpg'
+import background from '../images/background.jpg'
 
 export const WrapperApp = styled.div`
   position: relative;
